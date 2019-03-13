e61267207f94be9bf0a5647b17612ce0e52d11d7
Quentin <quentinbzl31@gmail.com>
2019-03-13 22:46:41 +0100

    barre progression

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Product } from '../world';
 import { apiUrl } from './api';
+
+declare var require;
+const ProgressBar = require('progressbar.js');
 
 @Component({
   selector: 'app-product',
@@ -10,6 +13,10 @@ import { apiUrl } from './api';
 export class ProductComponent implements OnInit {
 
   product: Product;
+
+  @ViewChild('bar') progressBarItem;
+  progressbar: any;
+
   @Input()
   set prod(value: Product) {
     this.product = value;
@@ -21,6 +28,16 @@ export class ProductComponent implements OnInit {
     return apiUrl + this.product.logo;
   }
 
+  startFabrication() {
+    this.progressbar.set(0);
+    this.progressbar.animate(1, { duration: this.product.vitesse });
+  }
+
   ngOnInit() {
+    this.progressbar = new ProgressBar.Line(this.progressBarItem.nativeElement, {
+      strokeWidth: 50, color:
+        '#00ff00'
+    });
+    this.progressbar.animate(1, { duration: this.product.vitesse });
   }
 }
